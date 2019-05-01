007c3664c9ac8268b97e89cd79c4f21f7a024169
jouncous <42743231+jouncous@users.noreply.github.com>
2019-05-01 22:25:01 +0800

    Add files via upload

---
+++ src/login/Register.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+/**
+ *
+ * @author user
+ */
+public class Register {
+    
+    private int id;
+    private String fullname,username,password,type;
+
+    public Register(int id, String fullname, String username, String password, String type){
+        
+        this.id=id;
+        this.fullname=fullname;
+        this.username=username;
+        this.password=password;
+        this.type=type;
+        
+    }
+    public int getid(){
+        return id;
+    }
+    public String getfullname(){
+        return fullname;
+    }
+    public String getusername(){
+        return username;
+    }
+    public String getpassword(){
+        return password;
+    }
+    public String gettype(){
+        return type;
+    }
+}
